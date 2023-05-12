24ccc80179960f9f3e8d537e28ac511dabeb2d60
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2023-05-12 23:06:48 +0530

    diagonal sum of a square matrix

---
+++ matrix/diagonal_sum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// https://leetcode.com/problems/matrix-diagonal-sum/
+
+int diagonal_sum(vector<vector<int>> &matrix){
+    int sum = 0;
+    int len = matrix.size();
+    for(int i = 0;i < len;i++){
+        sum += matrix[i][i];
+        sum += matrix[len-i-1][i];
+    }
+    if(len%2 != 0){
+        sum -= matrix[len/2][len/2];
+    }return sum;
+}
+
+int main(){
+    vector<vector<int>> matrix = {{1,2,3},{4,5,6},{7,8,9}};
+    cout<<diagonal_sum(matrix)<<endl;
+    return 0;
+}
